52f60aa63e85385ac301b3a50bd7249369824a52
Didanghoc <nguyen.david.duyy@gmai.com>
2022-11-27 12:01:48 +0700

    code hcn

---
+++ main.js
@@ -0,0 +1,27 @@
+// let i=7
+// while (i>=0){
+//     console.log(i)
+//     console.log("hello")
+// i--
+// }
+
+// for(i=7; i >=0; i--){
+//     console.log(i)
+//     console.log("hello world")
+// }
+
+// for(i=7; i>0; i--){
+//     console.log(i)
+//     document.getElementById("my-list").innerHTML+='<ul>bla</ul>'
+// }
+
+for (let i = 0; i < 9; i++) {
+  for (let j = 0; j < 7; j++) {
+    //  document.getElementById("my-text").innerHTML+='<p>*</p>'
+    document.writeln("* ");
+  }
+  document.writeln("<br/>")
+}
+
+
+
